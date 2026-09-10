048e989eac0d16ae4de509c6c8f60d58b5014aca
kdxiaoyi [MacMini] <kdxiaoyi@outlook.com>
2026-09-10 22:54:33 +0800

    fix: 修复 NeonaicNewable 类中的实例 ID 生成逻辑，使用全局唯一 ID 函数

--- src/system/NeonaicNewableClass.js
+++ src/system/NeonaicNewableClass.js
@@ -1,11 +1,9 @@
-import { getLogger } from "./logger/Logger.js";
-
 /**
  * @abstract Neonaic可实例化类的通用基类
  * @internalApi For Neonaic only
  */
 export class NeonaicNewable {
-  #INSTANCE_ID = new UUID(); #TIMESTAMP = new Date();
+  #INSTANCE_ID = getUniqueId(); #TIMESTAMP = new Date();
   get INSTANCE_ID() { return this.#INSTANCE_ID; };
   get TIMESTAMP() { return this.#TIMESTAMP; };
 
@@ -17,9 +15,10 @@ export class NeonaicNewable {
 const BASENUM = 34699705549214;
 let count = Math.abs(BASENUM - Math.floor(Math.random() * 1e14));
 
+/** @returns 全局唯一的 number 数据 */
 export function getUniqueId() {
   if (count < Number.MAX_SAFE_INTEGER) return count++;
+  // 静默回绕
   count = 0;
-  getLogger().main.error("内部计数器已达到最大值", Number.MAX_SAFE_INTEGER, "，并发生回绕。这可能会带来业务问题。");
   return count++;
 }
