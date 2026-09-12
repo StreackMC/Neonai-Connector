ffbdc75dadb60c95e01eda2da47b6f39bed31820
kdxiaoyi [MacMini] <kdxiaoyi@outlook.com>
2026-09-12 10:29:14 +0800

    refactor: 优化代码导出

--- src/system/NeonaicNewableClass.js
+++ src/system/NeonaicNewableClass.js
@@ -3,26 +3,26 @@
  * @internalApi For Neonaic only
  */
 export class NeonaicNewable {
-  #INSTANCE_ID = getUniqueId(); #TIMESTAMP = new Date();
+  #INSTANCE_ID = getuid(); #TIMESTAMP = new Date();
   get INSTANCE_ID() { return this.#INSTANCE_ID; };
   get TIMESTAMP() { return this.#TIMESTAMP; };
 
   constructor() {
     if (new.target === NeonaicNewable) throw new Error("NeonaicNewable is a raw class, which is not allowed to be create directly");
   }
+
+  /** @returns 全局唯一的 number 数据 */
+  static getUniqueId() {
+    return getuid();
+  }
 }
 
 const BASENUM = 34699705549214;
 let count = Math.abs(BASENUM - Math.floor(Math.random() * 1e14));
 
-/** @returns 全局唯一的 number 数据 */
-function getUniqueId() {
+function getuid() {
   if (count < Number.MAX_SAFE_INTEGER) return count++;
   // 静默回绕
   count = 0;
   return count++;
 }
-
-export const NeonaicNewableClass = {
-  getUniqueId,
-};
